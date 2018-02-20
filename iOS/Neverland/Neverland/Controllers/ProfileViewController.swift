1101f1b0ee9f7a8d82d41b61ce5614bc4e79b80b
kosapo <k.sapa1997@gmail.com>
2018-02-20 22:55:21 +0200

    small UI changes

--- iOS/Neverland/Neverland/Controllers/ProfileViewController.swift
+++ iOS/Neverland/Neverland/Controllers/ProfileViewController.swift
@@ -10,26 +10,35 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let width = (view.frame.size.width - 30) / 3
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - CollectionViewProtocols
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
--- iOS/Neverland/Neverland/Controllers/QuestDetailedViewController.swift
+++ iOS/Neverland/Neverland/Controllers/QuestDetailedViewController.swift
@@ -10,26 +10,37 @@ import UIKit
 
 class QuestDetailedViewController: UIViewController {
 
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 3 columns - 5px margin of main view ( 5 * 2) = 10 + 10px between images = 2 * 10 = 20 px. 30 px.
+        let width = (view.frame.size.width - 30) / 3
+        let layout = photoCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
 
-        // Do any additional setup after loading the view.
+        photoCollectionView.delegate = self
+        photoCollectionView.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+// MARK: - CollectionViewProtocols
+extension QuestDetailedViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
+
+
+
