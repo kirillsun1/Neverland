92f4e0b6c41cfc562d1b7bfd2ecc230e6e750382
Nata <nabarata@gmail.com>
2018-03-25 19:07:26 +0300

    changed path to pictures

--- Server/Neverland/src/main/java/ee/knk/neverland/controller/FileUploadController.java
+++ Server/Neverland/src/main/java/ee/knk/neverland/controller/FileUploadController.java
@@ -20,6 +20,7 @@ public class FileUploadController {
     private final TokenController tokenController;
     private final ProofController proofController;
     private Gson gson = new Gson();
+    private String standardPath = "/var/www/html/never_pictures/proofs/";
 
     @Autowired
     public FileUploadController(TokenService tokenService, ProofController proofController) {
@@ -27,21 +28,36 @@ public class FileUploadController {
         this.proofController = proofController;
     }
 
-    @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("token") String token, @RequestParam("qid") Long questId,
-                                                 @RequestParam("file") MultipartFile file, @RequestParam("comment") String comment){
-        String standardPath = "http://vrot.bounceme.net:8081/never_pictures/proofs/";
+    @RequestMapping(value="/upload", method=RequestMethod.GET)
+    public @ResponseBody String provideUploadInfo(@RequestParam("token") String token) {
         Optional<User> user = tokenController.getTokenUser(token);
         if (!user.isPresent()) {
             return gson.toJson(new StandardAnswer(Constants.FAILED));
         }
-        String path = standardPath + user.get().getUsername() + "_" + questId + ".jpg";
-        proofController.addProof(questId, user.get(), path, comment);
+        String username = user.get().getUsername();
+        Path path = Paths.get(standardPath + username);
+        if (Files.exists(path)) {
+            return gson.toJson(new StandardAnswer(Constants.FAILED));
+        }
+        return gson.toJson(new StandardAnswer(Constants.SUCCEED));
+    }
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("token") String token, @RequestParam("qid") Long questId,
+                                                 @RequestParam("file") MultipartFile file, @RequestParam("comment") String comment){
+        String realPath = "/var/www/html/never_pictures/proofs/";
+        String dbPath = "http://vrot.bounceme.net:8081/never_pictures/proofs/";
+        Optional<User> user = tokenController.getTokenUser(token);
+        if (!user.isPresent()) {
+            return gson.toJson(new StandardAnswer(Constants.FAILED));
+        }
+        String pathEnding = user.get().getUsername() + "_" + questId + ".jpg";
+        proofController.addProof(questId, user.get(), dbPath + pathEnding, comment);
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(path)));
+                        new BufferedOutputStream(new FileOutputStream(new File(realPath + pathEnding)));
                 stream.write(bytes);
                 stream.close();
                 return gson.toJson(new StandardAnswer(Constants.SUCCEED));
