2f7fe32b5710fd0e4b95b345a1e43073e939008d
Nata <nabarata@gmail.com>
2018-05-12 17:15:38 +0300

    utf-8?

--- Server/Neverland/src/main/java/ee/knk/neverland/config/DataConfig.java
+++ Server/Neverland/src/main/java/ee/knk/neverland/config/DataConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
@@ -25,6 +28,16 @@ public class DataConfig {
         factory.setMaxFileSize("5000KB");
         factory.setMaxRequestSize("5000KB");
         return factory.createMultipartConfig();
+    }
+
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        return filter;
     }
 
 
